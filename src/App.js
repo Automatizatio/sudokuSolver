921b3c5c8fe3a957fcc87be0eeb79805086221a5
chrisnnchris <chrisnnchris@gmail.com>
2023-05-25 17:41:11 -0700

    Fixed the same value column problem

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
-import React from 'react';
+import React, { useState } from "react";
 
-class Grid extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      grid: Array(9).fill().map(() => Array(9).fill(null))
-    };
-  }
+const Grid = () => {
+  // Initialize an empty 9x9 grid
+  const [grid, setGrid] = useState(Array(9).fill().map(() => Array(9).fill("")));
 
-  handleClick() {
-    console.log(this.state.grid);
-  }
+  // Handle the change of a cell value
+  const handleChange = (e, row, col) => {
+    // Get the new value and validate it
+    let value = e.target.value;
+    if (value === "" || (value >= 1 && value <= 9)) {
+      // Copy the grid and update the cell
+      let newGrid = [...grid]
+      newGrid[row][col] = value;
+      // Set the new grid state
+      setGrid(newGrid);
+    }
+    console.log(grid)
+  };
 
-  render() {
-    return (
-      <div>
-        {this.state.grid.map((row, i) => (
-          <div key={i}>
-            {row.map((cell, j) => (
-              <input
-                key={j}
-                type="number"
-                value={cell}
-                onChange={e => {
-                  const grid = this.state.grid.slice();
-                  grid[i][j] = parseInt(e.target.value, 10);
-                  this.setState({grid});
-                }}
-              />
-            ))}
-          </div>
-        ))}
-        <button onClick={() => this.handleClick()}>Submit</button>
-      </div>
-    );
-  }
-}
+  // Handle the click of the first button
+  const handleClick1 = () => {
+    // Log the grid as a 2D array
+    console.log(grid);
+  };
+
+  // Handle the click of the second button
+  const handleClick2 = (newGrid) => {
+    // Set the grid to the parameter
+    console.log('before grid change')
+    console.log(grid)
+    grid[0][5] = '6'
+    console.log('after grid change')
+    console.log(grid)
+    let newGrid1 = [...grid]
+    
+    // Set the new grid state
+    setGrid(newGrid1);
+
+    //setGrid(grid)
+  };
+
+  return (
+    <div className="grid">
+      {/* Render the grid as a table */}
+      <table>
+        <tbody>
+          {grid.map((row, i) => (
+            <tr key={i}>
+              {row.map((cell, j) => (
+                <td key={j}>
+                  {/* Use an input element for each cell */}
+                  <input
+                    type="text"
+                    value={cell}
+                    onChange={(e) => handleChange(e, i, j)}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {/* Render the buttons */}
+      <button onClick={handleClick1}>Get grid as 2D array</button>
+      <button onClick={() => handleClick2([[1,2,3],[4,5,6],[7,8,9]])}>Set grid</button>
+    </div>
+  );
+};
 
 export default Grid;
-
